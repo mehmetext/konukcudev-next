b5fba1cd1755c326825ac607ee4acaeb33bb8ba4
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 02:09:46 +0300

    Icon'a key eklendi

--- src/components/molecules/WelcomeSection/TechStack.tsx
+++ src/components/molecules/WelcomeSection/TechStack.tsx
@@ -23,8 +23,12 @@ export default function TechStack() {
 	return (
 		<ComponentNameContainer name="TechStack">
 			<div className="flex flex-wrap justify-center gap-1 sm:gap-1.5 md:gap-2.5">
-				{technologies.map((tech) => (
-					<Icon name={tech} className="h-6 w-6 md:h-8 md:w-8 lg:h-12 lg:w-12" />
+				{technologies.map((tech, i) => (
+					<Icon
+						key={i}
+						name={tech}
+						className="h-6 w-6 md:h-8 md:w-8 lg:h-12 lg:w-12"
+					/>
 				))}
 			</div>
 		</ComponentNameContainer>
