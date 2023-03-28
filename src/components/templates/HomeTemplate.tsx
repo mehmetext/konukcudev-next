760771b32f96a8ec1aa599fb12453e55caf2deca
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-28 18:44:01 +0300

    Atomic design yapısı kuruldu, HomeTemplate eklendi

---
+++ src/components/templates/HomeTemplate.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function HomeTemplate() {
+	return <div>HomeTemplate</div>;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,19 @@
+//Components
+import HomeTemplate from "@/components/templates/HomeTemplate";
+
+//Next
+import Head from "next/head";
+
 export default function Home() {
-	return <div>Home</div>;
+	return (
+		<>
+			<Head>
+				<title>Mehmet Konukçu - Personal Website</title>
+				<meta name="description" content="Mehmet Konukçu - Personal Website" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<HomeTemplate />
+		</>
+	);
 }
