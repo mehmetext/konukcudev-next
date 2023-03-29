2fae064f880494e094877811f6fd60ffabe3548e
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 04:43:01 +0300

    DateLineBlock title eklendi

--- src/components/molecules/ExperiencesEducation/DateLineBlock.tsx
+++ src/components/molecules/ExperiencesEducation/DateLineBlock.tsx
@@ -5,6 +5,7 @@ import DateLineBlockDate from "@/components/atoms/DateLineBlock/DateLineBlockDat
 type Props = {
 	endingDate?: DateLineDateType;
 	startingDate: DateLineDateType;
+	title: string;
 };
 
 type DateLineDateType = {
@@ -12,11 +13,15 @@ type DateLineDateType = {
 	year: string;
 };
 
-export default function DateLineBlock({ endingDate, startingDate }: Props) {
+export default function DateLineBlock({
+	endingDate,
+	startingDate,
+	title,
+}: Props) {
 	return (
 		<div className="flex gap-x-2.5">
 			<div className="flex flex-col gap-y-2.5">
-				<div className="shrink-0 flex flex-col py-2 gap-px bg-primary text-white rounded-full min-w-[48px] items-center justify-center">
+				<div className="shrink-0 flex flex-col py-2 gap-px bg-gradient-to-r from-primary-dark to-primary text-white rounded-full min-w-[48px] items-center justify-center">
 					{endingDate && (
 						<>
 							<DateLineBlockDate
@@ -33,7 +38,9 @@ export default function DateLineBlock({ endingDate, startingDate }: Props) {
 				</div>
 				<div className="w-px flex-1 bg-red-500"></div>
 			</div>
-			<div className="">adsa</div>
+			<div className="flex flex-col gap-[5px]">
+				<div className="text-4xl font-black mt-1">{title}</div>
+			</div>
 		</div>
 	);
 }
--- src/components/molecules/ExperiencesEducation/Experiences.tsx
+++ src/components/molecules/ExperiencesEducation/Experiences.tsx
@@ -7,8 +7,13 @@ export default function Experiences() {
 		<ComponentNameContainer name="Experiences">
 			<div className="flex flex-col gap-y-2.5">
 				<DateLineBlock
+					startingDate={{ month: "07", year: "2023" }}
+					title="Frontend Developer"
+				/>
+				<DateLineBlock
 					endingDate={{ month: "09", year: "2022" }}
 					startingDate={{ month: "07", year: "2022" }}
+					title="Mobile Application Developer"
 				/>
 			</div>
 		</ComponentNameContainer>
