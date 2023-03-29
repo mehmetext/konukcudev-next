80808d7b0cd77b31a25645fd4315f68c81b18d5e
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 04:38:46 +0300

    DateLineBlockDate dinamikleştirildi

--- src/components/molecules/ExperiencesEducation/DateLineBlock.tsx
+++ src/components/molecules/ExperiencesEducation/DateLineBlock.tsx
@@ -1,11 +1,35 @@
+//Components
 import DateLineBlockDate from "@/components/atoms/DateLineBlock/DateLineBlockDate";
 
-export default function DateLineBlock() {
+//Types
+type Props = {
+	endingDate?: DateLineDateType;
+	startingDate: DateLineDateType;
+};
+
+type DateLineDateType = {
+	month: string;
+	year: string;
+};
+
+export default function DateLineBlock({ endingDate, startingDate }: Props) {
 	return (
 		<div className="flex gap-x-2.5">
 			<div className="flex flex-col gap-y-2.5">
 				<div className="shrink-0 flex flex-col py-2 gap-px bg-primary text-white rounded-full min-w-[48px] items-center justify-center">
-					<DateLineBlockDate month="09" year="2023" />
+					{endingDate && (
+						<>
+							<DateLineBlockDate
+								month={endingDate.month}
+								year={endingDate.year}
+							/>
+							<div className="h-2.5 w-px bg-white rounded-full"></div>
+						</>
+					)}
+					<DateLineBlockDate
+						month={startingDate.month}
+						year={startingDate.year}
+					/>
 				</div>
 				<div className="w-px flex-1 bg-red-500"></div>
 			</div>
--- src/components/molecules/ExperiencesEducation/Experiences.tsx
+++ src/components/molecules/ExperiencesEducation/Experiences.tsx
@@ -6,8 +6,10 @@ export default function Experiences() {
 	return (
 		<ComponentNameContainer name="Experiences">
 			<div className="flex flex-col gap-y-2.5">
-				<DateLineBlock />
-				<DateLineBlock />
+				<DateLineBlock
+					endingDate={{ month: "09", year: "2022" }}
+					startingDate={{ month: "07", year: "2022" }}
+				/>
 			</div>
 		</ComponentNameContainer>
 	);
