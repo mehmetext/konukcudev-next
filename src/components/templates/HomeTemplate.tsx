09cbded703316b26d2548c34ba7d4d4ebf2a323e
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 18:00:49 +0300

    Email eklendi

--- src/components/templates/HomeTemplate.tsx
+++ src/components/templates/HomeTemplate.tsx
@@ -3,6 +3,7 @@ import Footer from "../organisms/Footer";
 import AsDeveloper from "../organisms/HomeTemplate/AsDeveloper";
 import BiKodist from "../organisms/HomeTemplate/BiKodist";
 import DesireToLearn from "../organisms/HomeTemplate/DesireToLearn";
+import Email from "../organisms/HomeTemplate/Email";
 import ExperiencesEducation from "../organisms/HomeTemplate/ExperiencesEducation";
 import Others from "../organisms/HomeTemplate/Others";
 import SocialMedia from "../organisms/HomeTemplate/SocialMedia";
@@ -22,6 +23,7 @@ export default function HomeTemplate() {
 			<WhatDidIDo />
 			<SocialMedia />
 			<BiKodist />
+			<Email />
 			<Footer />
 		</div>
 	);
