1b373c15967b98f86367cc89171acc338903e140
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 14:54:11 +0300

    AsDeveloperItem renkler ve TechStack boyut düzeltildi

--- src/components/molecules/AsDeveloper/AsDeveloperItem.tsx
+++ src/components/molecules/AsDeveloper/AsDeveloperItem.tsx
@@ -33,20 +33,21 @@ export default function AsDeveloperItem({
 					className={`flex gap-5 ${reversed ? "flex-row-reverse" : "flex-row"}`}
 				>
 					<div className="flex-1 flex flex-col gap-2.5">
-						<h1 className="font-black text-2xl">
+						<h1 className="font-black text-2xl text-primary-dark">
 							Bir <span className={`text-4xl ${textColor}`}>{name}</span>{" "}
 							olarak...
 						</h1>
-						<div className="flex flex-col gap-4">{paragraphs}</div>
+						<div className="flex flex-col gap-4 text-darken">{paragraphs}</div>
 						<div
 							className={`flex flex-col p-4 gap-2.5 bg-opacity-5 rounded-2xl ${bgColor}`}
 						>
-							<h2 className="text-2xl font-black">
+							<h2 className="text-2xl font-black text-primary-dark">
 								{name} olarak şu teknolojileri kullanıyorum:
 							</h2>
 							<TechStack
-								itemClassName="h-6 w-6 md:h-8 md:w-8 lg:h-12 lg:w-12"
+								itemClassName="h-9 w-9"
 								items={technologies}
+								gapClassName="gap-2.5"
 							/>
 						</div>
 					</div>
--- src/components/molecules/TechStack.tsx
+++ src/components/molecules/TechStack.tsx
@@ -3,18 +3,20 @@ import Icon from "@/components/atoms/Icon";
 //Types
 type Props = {
 	itemClassName?: string;
+	gapClassName?: string;
 	items: string[];
 	centered?: boolean;
 };
 
 export default function TechStack({
 	itemClassName,
+	gapClassName = "gap-1 sm:gap-1.5 md:gap-2.5",
 	items,
 	centered = false,
 }: Props) {
 	return (
 		<div
-			className={`flex flex-wrap gap-1 sm:gap-1.5 md:gap-2.5 ${
+			className={`flex flex-wrap ${gapClassName} ${
 				centered ? "justify-center" : ""
 			}`}
 		>
