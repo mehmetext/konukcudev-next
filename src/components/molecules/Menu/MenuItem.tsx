020a3f1bd0b0fec9cc362c33dc93edcdac2c93e3
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 18:31:24 +0300

    MenuItem href eklendi

--- src/components/molecules/Menu/MenuItem.tsx
+++ src/components/molecules/Menu/MenuItem.tsx
@@ -16,7 +16,7 @@ export default function MenuItem({ href, children }: Props) {
 
 	return (
 		<Link
-			href="/"
+			href={href}
 			className={`shrink-0 flex items-center justify-center text-center font-black gap-1 py-3 px-4 rounded-full ${
 				isActive
 					? "bg-gradient-to-r from-primary-dark to-primary text-white"
