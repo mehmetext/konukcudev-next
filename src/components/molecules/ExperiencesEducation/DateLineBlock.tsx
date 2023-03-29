e4eff9f721cf2548efa13018950ea51a462c90da
Mehmet Konukcu <mehmetext68@gmail.com>
2023-03-29 05:05:49 +0300

    Experiences eklendi

--- src/components/molecules/ExperiencesEducation/DateLineBlock.tsx
+++ src/components/molecules/ExperiencesEducation/DateLineBlock.tsx
@@ -1,11 +1,25 @@
 //Components
 import DateLineBlockDate from "@/components/atoms/DateLineBlock/DateLineBlockDate";
 
+//Next
+import Link from "next/link";
+
+//Icons
+import { MdLocationPin } from "react-icons/md";
+import { TbLink } from "react-icons/tb";
+
 //Types
 type Props = {
 	endingDate?: DateLineDateType;
 	startingDate: DateLineDateType;
 	title: string;
+	hideLine?: boolean;
+	location?: string;
+	company?: {
+		href: string;
+		title: string;
+	};
+	msg: string;
 };
 
 type DateLineDateType = {
@@ -17,10 +31,14 @@ export default function DateLineBlock({
 	endingDate,
 	startingDate,
 	title,
+	location,
+	company,
+	msg,
+	hideLine = false,
 }: Props) {
 	return (
 		<div className="flex gap-x-2.5">
-			<div className="flex flex-col gap-y-2.5">
+			<div className="flex flex-col gap-y-2.5 items-center">
 				<div className="shrink-0 flex flex-col py-2 gap-px bg-gradient-to-r from-primary-dark to-primary text-white rounded-full min-w-[48px] items-center justify-center">
 					{endingDate && (
 						<>
@@ -36,10 +54,31 @@ export default function DateLineBlock({
 						year={startingDate.year}
 					/>
 				</div>
-				<div className="w-px flex-1 bg-red-500"></div>
+				{!hideLine && (
+					<div className="w-px flex-1 border-l border-dashed border-l-darken"></div>
+				)}
 			</div>
 			<div className="flex flex-col gap-[5px]">
 				<div className="text-4xl font-black mt-1">{title}</div>
+				{(location || company) && (
+					<div className="flex gap-x-2.5">
+						{location && (
+							<div className="flex items-center">
+								<MdLocationPin className="text-primary" />
+								<span className="font-normal text-sm">{location}</span>
+							</div>
+						)}
+						{company && (
+							<div className="flex items-center gap-x-px">
+								<TbLink className="text-primary" />
+								<Link href="/" className="font-black text-sm">
+									Komünite
+								</Link>
+							</div>
+						)}
+					</div>
+				)}
+				<div className="text-darken text-sm">{msg}</div>
 			</div>
 		</div>
 	);
--- src/components/molecules/ExperiencesEducation/Experiences.tsx
+++ src/components/molecules/ExperiencesEducation/Experiences.tsx
@@ -7,13 +7,31 @@ export default function Experiences() {
 		<ComponentNameContainer name="Experiences">
 			<div className="flex flex-col gap-y-2.5">
 				<DateLineBlock
-					startingDate={{ month: "07", year: "2023" }}
+					startingDate={{ month: "03", year: "2023" }}
 					title="Frontend Developer"
+					location="Remote"
+					company={{
+						href: "https://komunite.com.tr",
+						title: "Komünite",
+					}}
+					msg="Komünite’de React.js, Next.js ve Tailwind CSS teknolojileriyle Frontend Developer olarak çalışmaktayım. Genellikle Figma’da çizilmiş olan UI’ları koda döküyorum."
 				/>
 				<DateLineBlock
 					endingDate={{ month: "09", year: "2022" }}
 					startingDate={{ month: "07", year: "2022" }}
 					title="Mobile Application Developer"
+					location="Konya, Türkiye"
+					company={{
+						href: "https://www.abranero.com",
+						title: "Abranero",
+					}}
+					msg="Üniversite staj dönemi kapsamında stajyer olarak çalıştım. Bu süreçte Flutter ile uygulamalar geliştirmeyi deneyimledim."
+				/>
+				<DateLineBlock
+					startingDate={{ month: "09", year: "2019" }}
+					title="Freelance Software Developer"
+					msg="Vaktim olduğu sürece kendi projelerimi geliştiriyorum. Ayrıca akrabalardan gelen “bana bi site yap” isteklerini de karşılamaya çalışıyorum :)"
+					hideLine
 				/>
 			</div>
 		</ComponentNameContainer>
