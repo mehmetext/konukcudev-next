2b8e71cc97fc49df2fbc7697013026df1bd20197
Mehmet Konukcu <mehmetext68@gmail.com>
2023-04-02 03:52:49 +0300

    responsive menü büyütüldü

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -17,17 +17,17 @@ export default function Menu({ sticky = true }: Props) {
 				sticky ? "sticky" : "fixed"
 			}`}
 		>
-			<div className="bg-black bg-opacity-5 backdrop-blur-md top-8 flex rounded-full gap-1 p-1 sm:gap-2.5 sm:p-2.5">
+			<div className="bg-black bg-opacity-5 backdrop-blur-md top-8 flex rounded-full gap-1.5 p-1.5 sm:gap-2.5 sm:p-2.5">
 				<MenuItem href="/">
-					<HiHome className="sm:text-2xl" />
+					<HiHome className="text-2xl" />
 					<span className="hidden sm:flex">Ana Sayfa</span>
 				</MenuItem>
 				<MenuItem href="/publications">
-					<MdMenuBook className="sm:text-2xl" />
+					<MdMenuBook className="text-2xl" />
 					<span className="hidden sm:flex">Yayınlar</span>
 				</MenuItem>
 				<MenuItem href="/references">
-					<HiTemplate className="sm:text-2xl" />
+					<HiTemplate className="text-2xl" />
 					<span className="hidden sm:flex">Referanslar</span>
 				</MenuItem>
 			</div>
