0a07ab52ca7d267df2ead91f182e29c57cb06d49
Mehmet Konukcu <mehmetext68@gmail.com>
2023-04-03 15:24:42 +0300

    Menu pointer-events özelliği eklendi

--- src/components/organisms/Menu.tsx
+++ src/components/organisms/Menu.tsx
@@ -13,11 +13,11 @@ type Props = {
 export default function Menu({ sticky = true }: Props) {
 	return (
 		<div
-			className={`w-full flex justify-center z-10 py-8 top-0 ${
+			className={`w-full flex justify-center z-10 py-8 top-0 pointer-events-none ${
 				sticky ? "sticky" : "fixed"
 			}`}
 		>
-			<div className="bg-black bg-opacity-5 backdrop-blur-md top-8 flex rounded-full gap-1.5 p-1.5 sm:gap-2.5 sm:p-2.5">
+			<div className="bg-black bg-opacity-5 backdrop-blur-md top-8 flex rounded-full gap-1.5 p-1.5 sm:gap-2.5 sm:p-2.5 pointer-events-auto">
 				<MenuItem href="/">
 					<HiHome className="text-2xl" />
 					<span className="hidden sm:flex">Ana Sayfa</span>
