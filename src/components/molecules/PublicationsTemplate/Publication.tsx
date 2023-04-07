bd44e9120f3f07d3913918987e4f96c62e21f75d
Mehmet <mehmetext68@gmail.com>
2023-04-07 03:12:35 +0300

    Update Publication.tsx

--- src/components/molecules/PublicationsTemplate/Publication.tsx
+++ src/components/molecules/PublicationsTemplate/Publication.tsx
@@ -24,7 +24,7 @@ export default function Publication({ icon, href, title, date }: Props) {
 				</Link>
 			</div>
 			<div className="hidden sm:flex items-start sm:pt-2.5">
-				<div className="text-sm text-[#636363] px-2.5 py-1 bg-bright rounded-full">
+				<div className="text-xs text-[#636363] px-2.5 py-1 bg-bright rounded-full">
 					{date}
 				</div>
 			</div>
