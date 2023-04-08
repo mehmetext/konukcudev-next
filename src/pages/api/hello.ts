fd8154a5ffdb5ab3bea71f4589dd9fd11f3faf0d
Mehmet <mehmetext68@gmail.com>
2023-04-09 02:59:24 +0300

    Update hello.ts

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-	name: string;
-};
-
-export default function handler(
-	req: NextApiRequest,
-	res: NextApiResponse<Data>
-) {
-	res.status(200).json({ name: "John Doe" });
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+	res.status(200).json({
+		str1: "Gizli bölgeye hoş geldin dostum.",
+		str2: "Yalnız, burada sana göre bi' şey yok :)",
+	});
 }
