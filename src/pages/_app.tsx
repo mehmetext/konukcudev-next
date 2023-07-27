038461c59a3364be115f715eafdad5ffed95182a
Mehmet <mehmetext68@gmail.com>
2023-07-27 14:49:03 +0300

    clear first setup files

--- src/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-//Styles
-import "@/styles/globals.css";
-
-//Next
-import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
-}
