02ed87cee94ec3af68375de36ad77f6bbf7b6d48
Mehmet <mehmetext68@gmail.com>
2023-07-27 14:59:27 +0300

    add images and tailwind configs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="">{children}</body>
+      <body className="font-nunito font-semibold">{children}</body>
     </html>
   );
 }
