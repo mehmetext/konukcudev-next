46e638fcbbd7f1ed995e87733cb47b5af684c05d
Mehmet <mehmetext68@gmail.com>
2023-07-27 22:51:41 +0300

    Update page.tsx

--- src/app/(index)/page.tsx
+++ src/app/(index)/page.tsx
@@ -2,8 +2,8 @@ import WelcomeSection from "./components/WelcomeSection";
 
 export default function Home() {
   return (
-    <>
+    <main className="flex flex-col gap-20">
       <WelcomeSection />
-    </>
+    </main>
   );
 }
