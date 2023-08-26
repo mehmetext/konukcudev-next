bb80a0e2f547831b2efcf52662699bd2a7b49cc3
Mehmet <mehmetext68@gmail.com>
2023-08-26 16:41:05 +0300

    Update OpacityFromTop.tsx

--- src/components/motions/OpacityFromTop.tsx
+++ src/components/motions/OpacityFromTop.tsx
@@ -10,8 +10,8 @@ export default function OpacityFromTop({
   return (
     <motion.main
       className="flex flex-col gap-20"
-      initial={{ transform: "translateY(-20px)", opacity: 0 }}
-      animate={{ transform: "translateY(0px)", opacity: 1 }}
+      initial={{ translateY: 30, opacity: 0 }}
+      animate={{ translateY: 0, opacity: 1 }}
     >
       {children}
     </motion.main>
