a492dd97ae506b6a522e229dcb8b3ee8dfe374e8
Mehmet <mehmetext68@gmail.com>
2023-08-26 16:11:01 +0300

    Update WelcomeSection.tsx

--- src/app/(index)/components/WelcomeSection.tsx
+++ src/app/(index)/components/WelcomeSection.tsx
@@ -4,7 +4,7 @@ import Continue from "./Continue";
 
 export default function WelcomeSection() {
   return (
-    <div className="w-screen h-screen flex flex-col items-center justify-center gap-3 px-4 sm:px-0">
+    <div className="relative w-screen h-screen flex flex-col items-center justify-center gap-3 px-4 sm:px-0">
       <ComponentNameContainer name="MehmetKonukcu">
         <div className="font-black text-[9vw] sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl text-transparent bg-clip-text bg-gradient-to-r from-primary to-primary-dark">
           {"<MehmetKonukçu/>"}
