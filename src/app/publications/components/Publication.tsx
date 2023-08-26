7dd6c7ca904b9cf8fdec8e5915d2b9b00da7df1c
Mehmet <mehmetext68@gmail.com>
2023-08-26 18:24:52 +0300

    Update Publication.tsx

--- src/app/publications/components/Publication.tsx
+++ src/app/publications/components/Publication.tsx
@@ -2,6 +2,7 @@
 
 import Icon from "@/components/Icon";
 import Link from "next/link";
+import { forwardRef } from "react";
 
 type Props = {
   icon: string;
@@ -10,22 +11,26 @@ type Props = {
   date: string;
 };
 
-export default function Publication({ icon, href, title, date }: Props) {
-  return (
-    <div className="flex gap-2.5">
-      <div className="shrink-0 flex h-6 w-6 sm:h-12 sm:w-12">
-        <Icon name={icon} className="w-full h-full" />
-      </div>
-      <div className="flex-1 sm:pt-2.5 items-start flex text-primary-dark font-black sm:text-xl">
-        <Link target="_blank" href={href}>
-          {title}
-        </Link>
-      </div>
-      <div className="flex items-start sm:pt-2.5">
-        <div className="text-xs text-[#636363] px-2.5 py-1 bg-bright rounded-full">
-          {date}
+const Publication = forwardRef<HTMLDivElement, Props>(
+  ({ icon, href, title, date }, ref) => {
+    return (
+      <div ref={ref} className="flex gap-2.5">
+        <div className="shrink-0 flex h-6 w-6 sm:h-12 sm:w-12">
+          <Icon name={icon} className="w-full h-full" />
+        </div>
+        <div className="flex-1 sm:pt-2.5 items-start flex text-primary-dark font-black sm:text-xl">
+          <Link target="_blank" href={href}>
+            {title}
+          </Link>
+        </div>
+        <div className="flex items-start sm:pt-2.5">
+          <div className="text-xs text-[#636363] px-2.5 py-1 bg-bright rounded-full">
+            {date}
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
+);
+
+export default Publication;
