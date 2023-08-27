87891551174ec11b6a2dbd8b3ee38308da907f9c
Mehmet <mehmetext68@gmail.com>
2023-08-27 15:36:49 +0300

    update publication responsive

--- src/app/publications/components/Publication.tsx
+++ src/app/publications/components/Publication.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import { forwardRef } from "react";
 
 import { motion } from "framer-motion";
+import cn from "@/lib/utils/cn";
 
 type Props = {
   icon: string;
@@ -30,19 +31,43 @@ const Publication = forwardRef<HTMLDivElement, Props>(
         <div className="shrink-0 flex h-6 w-6 sm:h-12 sm:w-12">
           <Icon name={icon} className="w-full h-full" />
         </div>
-        <div className="flex-1 sm:pt-2.5 items-start flex text-primary-dark font-black sm:text-xl">
-          <Link target="_blank" href={href}>
+        <div className="flex-1 sm:pt-2.5">
+          <Link
+            className="text-primary-dark font-black sm:text-xl"
+            target="_blank"
+            href={href}
+          >
             {title}
-          </Link>
+          </Link>{" "}
+          <DateComponent className="inline-flex sm:hidden">
+            ({date})
+          </DateComponent>
         </div>
-        <div className="flex items-start sm:pt-2.5">
-          <div className="text-xs text-[#636363] px-2.5 py-1 bg-bright rounded-full">
-            {date}
-          </div>
+        <div className="hidden sm:flex items-start sm:pt-2.5">
+          <DateComponent>{date}</DateComponent>
         </div>
       </motion.div>
     );
   }
 );
+
+export function DateComponent({
+  children,
+  className,
+}: {
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div
+      className={cn(
+        "text-xs text-[#636363] px-2.5 py-1 bg-bright rounded-full",
+        className
+      )}
+    >
+      {children}
+    </div>
+  );
+}
 
 export default Publication;
